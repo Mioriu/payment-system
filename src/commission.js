105fce30216d96aff062121fffa4f783c701c4e8
Miory <matthew.gg.dk@gmail.com>
2026-04-15 21:42:38 +0300

    Add progressive comission

--- src/commission.js
+++ src/commission.js
@@ -3,4 +3,14 @@ function calculateCommission(amount, rate) {
     return amount * rate;
 }
 
+
+function calculateProgressiveCommission(amount, rate) {
+    // TODO: implement progressive rates
+    if (amount < 100) {
+        return amount * rate;
+    } else if (amount < 1000) {
+        return amount * rate * 1.1;
+    }
+}
+
 module.exports = { calculateCommission };
