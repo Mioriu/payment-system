c64f4da8b2ad3ed1b1c2874094e6bd746cbd9134
Miory <matthew.gg.dk@gmail.com>
2026-04-15 21:14:35 +0300

    Add transaction validation

--- src/validation.js
+++ src/validation.js
@@ -7,4 +7,10 @@ function validateCurrency(currency) {
     return ['USD', 'EUR', 'GBP'].includes(currency);
 }
 
+
+function validateTransaction(transaction) {
+    return validateAmount(transaction.amount) && 
+           validateCurrency(transaction.currency);
+}
+
 module.exports = { validateAmount, validateCurrency };
